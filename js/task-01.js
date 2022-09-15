c993f182b7a585d700fc1b7971d9d2170c11ead4
Landar-S <landarsergij@gmail.com>
2022-09-15 23:15:40 +0300

    task-01

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const listCategories = document.querySelector("#categories");
+const allItemsCategories = document.querySelectorAll(".item");
+
+console.log(`Number of categories: ${listCategories.children.length}`);
+console.log("");
+
+allItemsCategories.forEach((elem) => {
+  console.log(`Category: ${elem.querySelector("h2").textContent}`);
+  console.log(`Elements: ${elem.querySelectorAll("li").length}`);
+  console.log("");
+});
