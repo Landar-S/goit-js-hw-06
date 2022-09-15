7e13c69601e3b5bea679f0d74a27fc8fd0863970
Landar-S <landarsergij@gmail.com>
2022-09-15 23:32:07 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listIngredients = document.querySelector("#ingredients");
+
+const newItems = ingredients.map((ingredient) => {
+  const newItem = document.createElement("li");
+
+  newItem.textContent = ingredient;
+  newItem.classList.add("item");
+
+  return newItem;
+});
+
+listIngredients.append(...newItems);
