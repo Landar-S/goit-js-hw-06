58dd9d8f4af432ae540f298d6c9f66757ef20430
Landar-S <landarsergij@gmail.com>
2022-09-16 23:04:17 +0300

    task-05

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const inputEl = document.querySelector("#name-input");
+const spanEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  spanEl.textContent = event.currentTarget.value.trim();
+
+  if (event.currentTarget.value === "") {
+    spanEl.textContent = "Anonymous";
+  }
+}
