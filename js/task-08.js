be7bb4c12ba07475fe44e71c1c5ebea259ea447a
Landar-S <landarsergij@gmail.com>
2022-09-18 13:50:17 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const formAutorization = document.querySelector(".login-form");
 
+formAutorization.addEventListener("submit", onAutorization);
+
+function onAutorization(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  const formData = new FormData(event.currentTarget);
+
+  if (!email.value || !password.value) {
+    alert("Please fill in all the fields!");
+  } else {
+    const data = {};
+
+    formData.forEach((value, name) => {
+      data[name] = value;
+    });
+
+    console.log(data);
+  }
+
+  event.currentTarget.reset();
+}
