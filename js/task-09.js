5f7744acdbb24598409221dfc978b8cc0915523f
Landar-S <landarsergij@gmail.com>
2022-09-18 14:26:41 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanEl = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+
+btnChangeColor.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  spanEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+}
